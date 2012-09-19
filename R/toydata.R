c54acfa7161514d14c486b70e5a2bd9918c05170
hp <lei@ubuntu>
2012-09-19 15:02:32 +0200

    metadata file added

--- R/toydata.R
+++ R/toydata.R
@@ -37,27 +37,57 @@ GenerateSimulatedData <- function (oligo.matrix.nolog.simulated, phylogeny.info,
   s <- colnames(oligo.matrix.nolog.simulated)
   colnames(oligo.matrix.nolog.simulated) <- paste("Sample-", 1:N, sep = "")
 
-  species.matrix.log10.simulated <- summarize.probesets(phylogeny.info, log10(oligo.matrix.nolog.simulated), "sum", "species", verbose = TRUE, phylotype.rm.list("HITChip"))
+  #species.matrix.log10.simulated <- summarize.probesets(phylogeny.info, log10(oligo.matrix.nolog.simulated), "sum", "species", verbose = TRUE, phylotype.rm.list("HITChip"))
 
   genus.matrix.log10.simulated <- summarize.probesets(phylogeny.info, log10(oligo.matrix.nolog.simulated), "sum", "L2", verbose = TRUE, phylotype.rm.list("HITChip"))
 
-  phylum.matrix.log10.simulated <- summarize.probesets(phylogeny.info, log10(oligo.matrix.nolog.simulated), "sum", "L1", verbose = TRUE, phylotype.rm.list("HITChip"))
+  #phylum.matrix.log10.simulated <- summarize.probesets(phylogeny.info, log10(oligo.matrix.nolog.simulated), "sum", "L1", verbose = TRUE, phylotype.rm.list("HITChip"))
 
   metadata.simulated <- data.frame(list(
               sampleID = colnames(oligo.matrix.nolog.simulated),
-	      subjectID = paste("subjectID", rep(1:4, 5)),
-	      group = sample(paste("group", rep(1:4, 5))),
-	      time = paste("group", rep(1:4, 5)),
+	      time = rep(1:4, 5),
               age = runif(N, 0, 100),
+              bmi = runif(N, 20, 40),
+	      subjectID = paste("subjectID", rep(1:4, 5), sep = "-"),
+	      group = sample(paste("group", rep(1:4, 5))),
               gender = sample(c("M", "F"), N, replace = TRUE),
               diet = sample(c("Apricots", "Beverages", "Carrots"), N, replace = TRUE)))
 
 
   message(paste("Saving the output in", output.file))
-  save(metadata.simulated, oligo.matrix.nolog.simulated, species.matrix.log10.simulated, genus.matrix.log10.simulated, phylum.matrix.log10.simulated, file = output.file, compress = "xz")
+  #save(metadata.simulated, oligo.matrix.nolog.simulated, species.matrix.log10.simulated, genus.matrix.log10.simulated, phylum.matrix.log10.simulated, file = output.file, compress = "xz")
+
+  save(metadata.simulated, oligo.matrix.nolog.simulated, genus.matrix.log10.simulated, file = output.file, compress = "xz")
 
   output.file
 
 }
 
 
+#' toydata-hitchip
+#'
+#' The toydata-hitchip contains the following simulated data matrices: 
+#'
+#' metadata.simulated sample metadata
+#' oligo.matrix.nolog.simulated: oligo-level data matrix (oligos vs. samples)
+#' genus.matrix.log10.simulated: genus (L2) level data matrix (phylotypes vs. samples)
+#'
+#' Generate with GenerateSimulatedData function
+#'
+#' @name toydata-hitchip
+#' @docType data
+#' @author Leo Lahti \email{microbiome-devel@@googlegroups.com}
+#' @references \url{microbiome.github.com}
+#' @keywords data
+NULL
+
+#' phylogeny
+#'
+#' The phylogeny contains the oligo-species-L2-L1 mappings for HITChip
+#'
+#' @name phylogeny
+#' @docType data
+#' @author Leo Lahti \email{microbiome-devel@@googlegroups.com}
+#' @references \url{microbiome.github.com}
+#' @keywords data
+NULL
