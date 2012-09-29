0c0b3a8343637e71d97ca9a3cb4fb32ba1fbc035
antagomir <lei@MIB12001>
2012-09-29 16:12:32 +0200

    added relative.abundance function

--- R/diversity.R
+++ R/diversity.R
@@ -202,6 +202,42 @@ evenness <- function (dat, det.th = NULL) {
 }
 
 
+#' relative.abundance
+#'
+#' Description: Estimate relative abundance for each phylotype in each sample with a given threshold
+#'
+#' Arguments:
+#'   @param dat data matrix (phylotypes x samples) in original (non-log) scale
+#'   @param det.th detection threshold. 
+#'
+#' Returns:
+#'   @return Vector containing relative proportions for each phylotype in each sample 
+#'
+#' @export
+#' @references See citation("microbiome") 
+#' @author Contact: Leo Lahti \email{microbiome-admin@@googlegroups.com}
+#' @keywords utilities
+
+relative.abundance <- function (dat, det.th = NULL) {
+
+  # Specify detection threshold if not provided
+  # Use the 80% quantile as this has proven robust across methodologies
+  if (is.null(det.th)) {
+    dat <- 10^t(impute(t(log10(dat)))) # impute missing values
+    det.th <- quantile(dat, 0.8)
+    warning(paste("Applying detection threshold at 0.8 quantile: ", det.th))
+  }
+
+  # Apply detection threshold
+  dat.th <- dat - det.th
+  dat.th[dat.th < 0] <- 0
+
+  # Species richness - count phylotypes that exceed detection threshold
+  dat <- apply(dat.th, 2, function (x) {x/sum(x)})
+
+  dat
+	
+}
 
 #' make.abundancy.table
 #'
