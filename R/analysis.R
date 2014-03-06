58c8666fa14853c9b48a64d9b2aa52a192da51db
antagomir <antagomir@lei>
2014-03-06 16:58:51 +0100

    meta

--- R/analysis.R
+++ R/analysis.R
@@ -208,6 +208,37 @@ distance.matrix <- function (x, method = "pearson", ...) {
 
 
 
+#' Description: Calculate ANOVA test (BH correction) for multi-group comparison. 
+#' Arguments:
+#'   @param dat data matrix (features x samples; eg. HITChip taxa vs. samples)
+#'   @param group Vector with specifying the groups
+#'   @param p.adjust.method p-value correction method for p.adjust function (default "BH"). If NULL, no correction will be performed.
+#'   @param sort sort the results
+#'
+#' Returns:
+#'   @return (Corrected) p-values for multi-group comparison.
+#'
+#' @examples data(peerj32); pval <- check.anova(t(peerj32$microbes), peerj32$meta$time)
+#' @export
+#'
+#' @references See citation("microbiome") 
+#' @author Contact: Leo Lahti \email{microbiome-admin@@googlegroups.com}
+#' @keywords utilities
+check.anova <- function (dat, group, p.adjust.method = "BH", sort = FALSE) {
+
+  pvals <- c()
+  for (tax in rownames(dat)) {
+    pvals[[tax]] <- anova(lm(dat[tax, ] ~ group))["group", "Pr(>F)"]
+  }
+
+  pvals <- p.adjust(pvals, method = p.adjust.method)
+
+  if (sort) {pvals <- sort(pvals)}
+
+  pvals
+}
+
+
 #' Description: Calculate Wilcoxon test (unpaired; BH correction) for the specified sample groups. 
 #' Either provide the input data as matrix, file path, or select the file through GUI.
 #'             
