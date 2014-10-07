802896b15f1cf474674e05bab6a2be6cc0cf1932
antagomir <antagomir@lei>
2014-10-07 14:32:29 +0200

    R

--- R/core.R
+++ R/core.R
@@ -1,17 +1,3 @@
-# Copyright (C) 2011-2014 Jarkko Salojarvi and Leo Lahti Contact:
-# <microbiome-admin@googlegroups.com>. All rights reserved.
-
-# This file is a part of the microbiome R package
-# http://microbiome.github.com/
-
-# This program is open source software; you can redistribute it and/or
-# modify it under the terms of the FreeBSD License (keep this notice):
-# http://en.wikipedia.org/wiki/BSD_licenses
-
-# This program is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-
 #' Description: core.which
 #'
 #' Arguments:
