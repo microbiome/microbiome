2e838286fb40a0c7859e4e74bbd1198b05a1002d
antagomir <leo.lahti@iki.fi>
2014-10-12 02:13:01 +0200

    ok

--- R/data.documentation.R
+++ R/data.documentation.R
@@ -16,7 +16,11 @@
 #'             indicated by integrated analysis of high-throughput profiling 
 #'             data. PeerJ 1:e32 \url{http://dx.doi.org/10.7717/peerj.32}
 #' @usage data(peerj32)
-#' @return List of data matrices
-#' @format list
+#' @format List of the following data matrices as described in detail in 
+#'         Lahti et al. (2013):
+#'   \item{lipids}{Quantification of 389 blood serum lipids across 44 samples}
+#'   \item{microbes}{Quantification of 130 genus-like taxa across 44 samples}
+#'   \item{meta}{Sample metadata including time point, gender, subjectID, sampleID and treatment group (probiotic LGG / Placebo)}
+#'   
 #' @keywords data
 NULL 
