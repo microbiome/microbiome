92409d53248d2f1fdcf0b2dd6e76f4b104b3f25c
antagomir <leo.lahti@iki.fi>
2015-05-17 01:15:52 +0200

    added files

---
+++ R/plot_abundance.R
@@ -0,0 +1,67 @@
+#' Plot taxon abundance for samples
+#'
+#' @param x \code{\link{phyloseq-class}} object or an OTU matrix (samples x phylotypes)
+#' @param taxonomic.level Merge the OTUs (for phyloseq object) into a higher taxonomic level. 
+#'                        This has to be one from \code{colnames(tax_table(x))}.
+#' @param relative.abundance Logical. Show relative abundances or not.
+#' @param sort.by Sort by sample data column. Or provide vector of sample IDs.
+#' @param x.label Specify how to label the x axis. This should be one of the variables in 
+#'        \code{sample_variables(x)}
+#'
+#' @return A \code{\link{ggplot}} plot object.
+#' 
+#' @import ggplot2
+#' @import reshape2
+#' @export
+#' @examples # 
+#' @keywords utilities
+plot_abundance <- function (x, taxonomic.level = NULL, relative.abundance = FALSE, sort.by = NULL, x.label = "sample") {
+
+  # Merge the taxa at a higher taxonomic level
+  if (!is.null(taxonomic.level)) {	         
+    x <- tax_glom(x, taxonomic.level)
+    # Fix the taxon names; tax_glom assigns wrong names
+  }
+
+  # Pick the OTU data
+  if (class(x) == "phyloseq") {
+    otu <- otu_table(x)@.Data
+    # FIXME: Remove this when this has been fixed to phyloseq package - pending 5/2015
+    if (!is.null(taxonomic.level)) {
+      rownames(otu) <- as.vector(tax_table(x)[, taxonomic.level])
+    }
+  } else {
+    otu <- t(x)
+  }
+
+  # Estimate relative abundances
+  if (relative.abundance) {
+    otu <- relative.abundance(otu)
+  }
+
+  # Define sample ordering based on the sort.by column
+  meta <- sample_data(x)
+  if (is.null(sort.by)) {
+    sort.by <- rownames(meta)    
+  } else if (length(sort.by) == 1) {
+    sort.by <- rownames(meta)[order(meta[[sort.by]])]
+  }
+
+  # Prepare data.frame
+  dfm <- melt(otu)
+  colnames(dfm) <- c("Taxon", "Sample", "Abundance")
+  dfm$Sample <- factor(as.character(dfm$Sample), levels = sort.by)
+  dfm$xlabel <- as.vector(unlist(meta[as.character(dfm$Sample), x.label]))
+
+  # Provide barplot of relative abundances
+  p <- ggplot(dfm, aes(x = Sample, y = Abundance, fill = Taxon))
+  p <- p + geom_bar(position = "stack", stat = "identity")
+  p <- p + scale_x_discrete(labels = dfm$xlabel, breaks = dfm$Sample)
+
+  # Rotate horizontal axis labels, and adjust
+  p <- p + theme(axis.text.x=element_text(angle=-90, vjust=0.5, hjust=0))
+
+  p
+
+}
+
---
+++ R/plot_density.R
@@ -0,0 +1,51 @@
+#' Plot taxon density for samples
+#'
+#' @param x \code{\link{phyloseq-class}} object or an OTU matrix (samples x phylotypes)
+#' @param otu.name OTU name to visualize
+#' @param log10 Logical. Show log10 abundances or not.
+#' @param adjust see stat_density
+#' @param kernel see stat_density
+#' @param trim see stat_density
+#' @param na.rm see stat_density
+#' @param fill Fill color
+#'
+#' @return A \code{\link{ggplot}} plot object.
+#' 
+#' @import ggplot2
+#' @export
+#' @examples # 
+#' @keywords utilities
+plot_density <- function (x, otu.name = NULL, log10 = FALSE, adjust = 1, kernel = "gaussian", trim = FALSE, na.rm = FALSE, fill = "gray") {
+
+  if (is.vector(x)) { 
+
+    df <- data.frame(x = x)
+    p <- ggplot(df, aes(x = x))
+    p <- p + geom_density(adjust = adjust, kernel = kernel, trim = trim, na.rm = na.rm, fill = fill)
+    
+    if (log10) {
+      #x <- log10(x)
+      p <- p + scale_x_log10()
+    }
+
+  } else if (class(x) == "phyloseq") { 
+
+    x <- otu_table(x)@.Data[otu.name,]
+    p <- plot_density(x, log10 = log10, adjust = adjust, kernel = kernel, trim = trim, na.rm = na.rm) 
+
+  }
+
+  if (log10) {
+    p <- p + ggtitle(paste(otu.name))
+    p <- p + xlab("Abundance (Log10)")
+  } else {
+    p <- p + ggtitle(paste(otu.name))
+    p <- p + xlab("Abundance (Abs.)")
+  }
+
+  p <- p + ylab("Frequency")
+
+  p
+
+}
+
