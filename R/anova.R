7069a9802e810d6a2b70177083a105d68e0964a2
antagomir <leo.lahti@iki.fi>
2015-05-17 21:12:02 +0200

    ok

--- R/anova.R
+++ R/anova.R
@@ -1,31 +1,32 @@
 #' Calculate ANOVA test (BH correction) for multi-group comparison 
 #'
-#' @param dat data matrix (features x samples; eg. HITChip taxa vs. samples)
+#' @param x \code{\link{phyloseq-class}} object or a data matrix 
+#'            (features x samples; eg. HITChip taxa vs. samples)
 #' @param group Vector with specifying the groups
 #' @param p.adjust.method p-value correction method for p.adjust function 
-#'               (default 'BH'). If NULL, no correction will be performed.
+#'               (default 'BH'). For other options, see ?p.adjust
 #' @param sort sort the results
 #'
 #' @return Corrected p-values for multi-group comparison.
 #'
 #' @examples 
 #'   data(peerj32)
-#'   pval <- check.anova(t(peerj32$microbes), peerj32$meta$time)
+#'   pval <- check_anova(t(peerj32$microbes), peerj32$meta$time)
 #'
 #' @export
 #'
 #' @references See citation('microbiome') 
 #' @author Contact: Leo Lahti \email{microbiome-admin@@googlegroups.com}
 #' @keywords utilities
-check.anova <- function(dat, group, p.adjust.method = "BH", sort = FALSE) {
-    
-    if (is.null(p.adjust.method)) {
-        p.adjust.method <- "none"
-    }
-    
+check_anova <- function(x, group, p.adjust.method = "BH", sort = FALSE) {
+
+  if (class(x) == "phyloseq") {    
+    x <- log10(otu_table(x)@.Data)
+  }
+
     pvals <- c()
-    for (tax in rownames(dat)) {
-        pvals[[tax]] <- anova(lm(dat[tax, ] ~ group))["group", "Pr(>F)"]
+    for (tax in rownames(x)) {
+        pvals[[tax]] <- anova(lm(x[tax, ] ~ group))["group", "Pr(>F)"]
     }
     
     pvals <- p.adjust(pvals, method = p.adjust.method)
@@ -35,4 +36,60 @@ check.anova <- function(dat, group, p.adjust.method = "BH", sort = FALSE) {
     }
     
     pvals
+}
+
+
+
+#' Calculate Wilcoxon test (with BH correction) for two-group comparison 
+#'
+#' @param x \code{\link{phyloseq-class}} object or a data matrix 
+#'            (features x samples; eg. HITChip taxa vs. samples)
+#' @param group Vector with specifying the groups
+#' @param p.adjust.method p-value correction method for p.adjust function 
+#'               (default 'BH'). For other options, see ?p.adjust
+#' @param sort sort the results
+#'
+#' @return Corrected p-values for multi-group comparison.
+#'
+#' @examples 
+#'   pseq <- download_microbiome("peerj32")$physeq
+#'   pval <- check_wilcoxon(pseq, "time")
+#'
+#' @export
+#'
+#' @references See citation('microbiome') 
+#' @author Contact: Leo Lahti \email{microbiome-admin@@googlegroups.com}
+#' @keywords utilities
+check_wilcoxon <- function (x, group, p.adjust.method = "BH", sort = FALSE) {
+
+  # Pick the grouping variable from sample metadata
+  g <- group
+  if (length(g) == 1) {
+    g <- sample_data(x)[[g]]
+    if (!is.factor(g)) {
+      stop(paste("The grouping variable", g, "must be a factor"))
+    }    
+    g <- droplevels(g)
+    if (!length(levels(g)) == 2) {
+      stop("check_wilcoxon is valid only for two-group comparisons")
+    }
+  }
+
+  if (class(x) == "phyloseq") {    
+    x <- log10(otu_table(x)@.Data)
+  }
+
+  # Calculate Wilcoxon test with BH p-value correction for gender
+  pval <- suppressWarnings(apply(x, 1, function (xi) {wilcox.test(xi ~ g)$p.value}))
+
+  # Multiple testing correction
+  pval <- p.adjust(pval, method = p.adjust.method)
+
+  # Sort p-values
+  if (sort) {
+    pval <- sort(pval)
+  }
+
+  pval
+
 }
--- vignettes/Comparisons.md
+++ vignettes/Comparisons.md
@@ -7,61 +7,32 @@ Read example data
 ```r
 # Example data
 library(microbiome)
-data(peerj32)
-x <- peerj32$microbes
-m <- peerj32$meta
+pseq <- download_microbiome("dietswap")
 ```
 
 ### Comparing of two or more groups with a parametric test (linear model; ANOVA)
 
 
 ```r
-# Define sample groups (gender + case/ctrl)
-group <- apply(cbind(m$gender, m$group), 1, function (x) {paste(x, collapse = "-")})
-names(group) <- m$sample
+# Define sample groups (gender + treatment group)
+meta <- sample_data(pseq)
+group <- meta$group
+names(group) <- meta$sample
 
-# Calculate p-values for multi-group comparison
-# NOTE: the HITChip data (x) must be at log10 scale in parametric tests!
-x <- log10(t(x))
-pvals <- check.anova(x, group, p.adjust.method = "BH", sort = TRUE)
-
-# Writing the results to a table
-# write.table(pvals, file = "myfile.tab", quote = F)
+# Calculate 1-way ANOVA p-values for the multi-group comparison
+pvals <- check_anova(pseq, group, p.adjust.method = "BH", sort = TRUE)
 ```
 
 
-### Wilcoxon test (only for two-group comparison)
+### Wilcoxon test (two-group comparisons)
 
 If the data remarkably violates Gaussian assumptions, you need to use
-non-parametric test, Wilcoxon is one option for two group
-comparison. Here we compare males and females in simulated toy
-data. The sampleIDs in G1 and G2 must match with input matrix
-columns. Compare two sample groups and return adjusted p-values for
-each phylotype. This test uses unpaired Wilcoxon test with BH
-correction.
+non-parametric test. Wilcoxon is one option for two group
+comparison. Here we compare males and females in the example data. 
 
 
 ```r
-# Calculate Wilcoxon test with BH p-value correction for gender
-pval <- apply(x, 1, function (xi) {wilcox.test(xi ~ m$gender)$p.value})
-
-# Multiple testing correction
-pval <- p.adjust(pval, "fdr")
-
-# Sort p-values
-pval <- sort(pval)
-
-# Investigate the top findings
-head(pval)
-```
-
-```
-##           Clostridium nexile et rel.         Megasphaera elsdenii et rel. 
-##                           0.04177601                           0.04177601 
-##          Uncultured Clostridiales II          Eubacterium siraeum et rel. 
-##                           0.04177601                           0.08356243 
-##        Dorea formicigenerans et rel. Outgrouping clostridium cluster XIVa 
-##                           0.08797056                           0.09743840
+pval <- check_wilcoxon(pseq, "gender")
 ```
 
 
