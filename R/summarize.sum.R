da1c2d5d506b0da3a253c7cff3a2f462568fdf98
antagomir <leo.lahti@iki.fi>
2015-05-22 10:39:25 -0400

    oik

--- R/summarize.sum.R
+++ R/summarize.sum.R
@@ -44,7 +44,7 @@ summarize.sum <- function (taxonomy, level, probedata, verbose = TRUE, downweigh
 
     # Pick probe data for the probeset: probes x samples
     # oligo.data assumed to be already in log10
-    dat <- as.matrix(oligo.data[probes,], nrow = length(probes)) 
+    dat <- matrix(oligo.data[probes,], nrow = length(probes)) 
     rownames(dat) <- probes
     colnames(dat) <- colnames(oligo.data)
 
