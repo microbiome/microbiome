685af8fed8a28fc7007e08024dee43ad1862e0f5
antagomir <leo.lahti@iki.fi>
2015-05-22 14:31:51 -0400

    testing

--- inst/extras/vm.R
+++ inst/extras/vm.R
@@ -3,31 +3,11 @@
 #fs <- list.files("~/microbiome/R/", full.names = TRUE)
 #for (f in fs)  { source(f) }
 
-# Mahdollisesti riittaa:
-# sudo apt-get update
-# sudo apt-get install libxml2-dev
-
-# sudo apt-get upgrade r-base
-# wget http://cran-mirror.cs.uu.nl/src/base/R-3/R-3.2.0.tar.gz
-# tar -zxvf R-3.2.0.tar.gz
-# ./configure
-# make
-# sudo make install
-# install.packages("devtools")
-
-source("http://www.bioconductor.org/biocLite.R")
-biocLite("WGCNA")
-biocLite("BiocStyle")
-biocLite("knitr")
-biocLite("rmarkdown")
-biocLite("phyloseq")
-biocLite("RPA")
-
-library(devtools)
-install_github("microbiome/microbiome")
-install_github("microbiome/HITChipDB")
-
 # -------------------------------------------
+
+git config --global user.email "leo.lahti@iki.fi"
+git config --global user.name "antagomir"
+
 
 library(HITChipDB)
 dbuser <- "root"
