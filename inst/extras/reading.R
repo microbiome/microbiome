3f022adc554c367775702b36bbe2e1cea68f1efc
antagomir <leo.lahti@iki.fi>
2015-06-18 09:57:53 -0400

    sync

--- inst/extras/reading.R
+++ inst/extras/reading.R
@@ -6,9 +6,15 @@ res.frpa <- read.profiling("output", method = "frpa")
 res.rpa <- read.profiling("output", method = "rpa")
 res.sum <- read.profiling("output", method = "sum")
 
-res <- read_hitchip("output", method = "sum")
+ressum <- read_hitchip("output", method = "sum")
+resrpa <- read_hitchip("output", method = "rpa")
+resfrpa <- read_hitchip("output", method = "frpa")
 
 #plot(log10(unlist(res.rpa$species)), log10(unlist(res.frpa$species)))
 #plot(log10(unlist(res.rpa$species)), log10(unlist(res.sum$species)))
 plot(log10(unlist(res$species)), log10(unlist(res.sum)))
+
+res1 <- res.rpa
+res2 <- resrpa
+plot(log10(unlist(res1$species)), log10(unlist(res2$abundance.table)))
 
