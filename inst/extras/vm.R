19bfa89835f2f1617baa004edbc9921b2472c189
antagomir <leo.lahti@iki.fi>
2015-06-23 11:02:24 -0400

    updates

--- inst/extras/vm.R
+++ inst/extras/vm.R
@@ -8,18 +8,6 @@ library(devtools)
 install_github("microbiome/microbiome")
 install_github("microbiome/HITChipDB")
 
-library(HITChipDB)
-dbuser <- "root"
-dbpwd <- "fidipro"
-dbname <- "phyloarray"
-verbose = TRUE
-host = '127.0.0.1'
-port = 3307
-summarization.methods = c("frpa", "sum", "rpa")
-which.projects = "AUTISM"
-
-#fs <- list.files("~/HITChipDB/R/", full.names = TRUE)
-#for (f in fs)  { source(f) }
 res <- run.profiling.script(dbuser, dbpwd, dbname, verbose = FALSE, host = host, port = port, summarization.methods = summarization.methods, which.projects = which.projects)
 
 projs <- list.mysql.projects(dbuser, dbpwd, dbname, host = host, port = port)
