748686c8872c75ff52fe6abea0491eaa855ce8ec
Leo Lahti <leo.lahti@iki.fi>
2016-04-22 11:47:09 +0200

    miving hitchip funcs to HITChipDB package

--- inst/extras/build.sh
+++ inst/extras/build.sh
@@ -1,6 +1,6 @@
 # https://support.rstudio.com/hc/en-us/articles/200486518-Customizing-Package-Build-Options
-/usr/bin/R CMD BATCH document.R
-/usr/bin/R CMD build ../../ --no-build-vignettes --no-examples
-/usr/bin/R CMD check microbiome_0.99.78.tar.gz --no-build-vignettes --no-examples
-/usr/bin/R CMD INSTALL microbiome_0.99.78.tar.gz 
-#/usr/bin/R CMD BiocCheck microbiome_0.99.32.tar.gz 
+/home/lei/bin/R CMD BATCH document.R
+/home/lei/bin/R CMD build ../../ --no-build-vignettes --no-examples
+/home/lei/bin/R CMD check microbiome_0.99.79.tar.gz --no-build-vignettes --no-examples
+/home/lei/bin/R CMD INSTALL microbiome_0.99.79.tar.gz 
+#/home/lei/bin/R CMD BiocCheck microbiome_0.99.32.tar.gz 
