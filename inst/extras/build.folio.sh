9901590c70cf06b09b0636426d12f0277021a595
antagomir <leo.lahti@iki.fi>
2016-04-27 11:46:37 +0200

    ok

---
+++ inst/extras/build.folio.sh
@@ -0,0 +1,6 @@
+# https://support.rstudio.com/hc/en-us/articles/200486518-Customizing-Package-Build-Options
+/usr/bin/R CMD BATCH document.R
+/usr/bin/R CMD build ../../ --no-build-vignettes --no-examples
+/usr/bin/R CMD check microbiome_0.99.81.tar.gz --no-build-vignettes --no-examples
+/usr/bin/R CMD INSTALL microbiome_0.99.81.tar.gz 
+#/use/bin/R CMD BiocCheck microbiome_0.99.32.tar.gz 
