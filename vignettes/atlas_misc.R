c9006a283f655e91c07c189533e103a6da581836
antagomir <leo.lahti@iki.fi>
2016-06-15 01:30:40 +0200

    ok

--- vignettes/atlas_misc.R
+++ vignettes/atlas_misc.R
@@ -1,8 +1,12 @@
+load("~/data/Atlas/20160604/AtlasFull.RData")
+
 library(plotly)
 library(microbiome)
 set.seed(100)
-m <- sample_data(atlas)
-x <- t(taxa_abundances(atlas))
+#m <- sample_data(atlas)
+m = atlas.metadata
+x = t(atlas$L2$rpa)
+#x <- t(taxa_abundances(atlas))
 pca = princomp(log10(x))$scores[, 1:2]
 d = data.frame(cbind(m, x, pca))
 d$nationality = as.character(d$nationality)
@@ -16,18 +20,19 @@ d$nationality[is.na(d$nationality)] = "Unknown"
 d$nationality = factor(d$nationality)
 #d = d[sample(1:nrow(d), 100), ]
 d = subset(d, !is.na(age) & !is.na(nationality))
-plot_ly(d, x = Comp.1, y = Comp.2, mode = "markers", color = nationality, size = age)
-plot_ly(d, x = Comp.1, y = Comp.2, mode = "markers", color = log10(d$Prevotella_melaninogenica_et_rel), size = age)
-plot_ly(d, x = Comp.1, y = Comp.2, mode = "markers", color = log10(Bacteroides_fragilis_et_rel), size = age)
-plot_ly(d, x = Comp.1, y = Comp.2, mode = "markers", color = age, size = age)
-
+#plot_ly(d, x = Comp.1, y = Comp.2, mode = "markers", color = nationality, size = age)
+#plot_ly(d, x = Comp.1, y = Comp.2, mode = "markers", color = log10(d$Prevotella_melaninogenica_et_rel), size = age)
+#plot_ly(d, x = Comp.1, y = Comp.2, mode = "markers", color = log10(Bacteroides_fragilis_et_rel), size = age)
+#plot_ly(d, x = Comp.1, y = Comp.2, mode = "markers", color = age, size = age)
 
 library(ggplot2)
 theme_set(theme_bw(30))
-ggplot(d, aes(x = Comp.1, y = Comp.2, color = DNA_extraction_method, size = age)) + geom_point()
-ggplot(d, aes(x = Comp.1, y = Comp.2, color = nationality, size = bmi_group)) + geom_point()
-ggplot(d, aes(x = Comp.1, y = Comp.2, color = health_status, size = bmi_group)) + geom_point()
-ggplot(d, aes(x = Comp.1, y = Comp.2, color = Prevotella_melaninogenica_et_rel, size = Prevotella_melaninogenica_et_rel)) + geom_point()
-ggplot(d, aes(x = Comp.1, y = Comp.2, color = Bacteroides_fragilis_et_rel, size = Bacteroides_fragilis_et_rel)) + geom_point()
+ggplot(d, aes(x = Comp.1, y = Comp.2, color = DNA.extr.meth, size = age)) + geom_point()
+ggplot(d, aes(x = Comp.1, y = Comp.2, color = nationality, size = bmi)) + geom_point()
+ggplot(d, aes(x = Comp.1, y = Comp.2, color = health.status, size = bmi)) + geom_point()
+ggplot(d, aes(x = Comp.1, y = Comp.2, color = sample.type, size = age)) + geom_point()
+ggplot(d, aes(x = Comp.1, y = Comp.2, color = Prevotella.melaninogenica.et.rel, size = Prevotella.melaninogenica.et.rel)) + geom_point()
+ggplot(d, aes(x = Comp.1, y = Comp.2, color = Bacteroides.fragilis.et.rel, size = Bacteroides.fragilis.et.rel)) + geom_point()
 ggplot(d, aes(x = Comp.1, y = Comp.2, color = Dialister, size = Dialister)) + geom_point()
+
 
