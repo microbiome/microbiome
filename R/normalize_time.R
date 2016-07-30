b122f293e1fb710c43fd502d9973d48eb5725c5d
antagomir <leo.lahti@iki.fi>
2016-07-30 14:42:25 +0300

    ok

---
+++ R/normalize_time.R
@@ -0,0 +1,26 @@
+#' @title Normalize Phyloseq Metadata Time Field
+#' @description Shift the time field in phyloseq sample_data such that the first time point of each subject is always 0.
+#' @param x phyloseq object
+#' @return Phyloseq object with a normalized time field
+#' @export
+#' @examples
+#'   library(microbiome)
+#'   data(atlas1006)
+#'   atlas1006b <- normalize_time(atlas1006)
+normalize_time <- function (x) {
+
+  x <- validate_phyloseq(x) 
+  meta <- sample_data(x)
+
+  # Shift the times such that the first time point is always 0
+  for (subj in unique(meta$subject)) {
+    inds <- which(meta$subject == subj & !is.na(meta$time))
+    meta[inds, "time"] <- meta[inds, "time"] - min(meta[inds, "time"])
+  }
+
+  # Fix this to sample metadata
+  x@sam_data <- meta
+    
+  # Return phyloseq with normalized time field
+  x
+}
---
+++ R/validate_phyloseq.R
@@ -0,0 +1,33 @@
+#' @title Validate Phyloseq
+#' @description Validate phyloseq object.
+#' @param x phyloseq object
+#' @details Checks that the otu_table and sample_data have exactly same samples.
+#' @return A validated and polished phyloseq object
+#' @export
+#' @examples
+#'   library(microbiome)
+#'   data(atlas1006)
+#'   validate_phyloseq(atlas1006)
+validate_phyloseq <- function (x) {
+
+  validated <- TRUE		  
+
+  dat <- t(taxa_abundances(x))
+  meta <- sample_data(x)		         
+  coms <- intersect(rownames(dat), rownames(meta))
+
+  if (length(coms) < 2) {
+    validated <- FALSE
+    warning("Check that the otu_table and sample_data have more than 1 samples in common")
+    return(validated)
+  } else {
+    # Include only the common samples    
+    x@sam_data <- meta[coms,]
+    otu_table(x) <- otu_table(t(dat[coms,]), taxa_are_rows = TRUE)
+  }
+
+  # Return validated and polished phyloseq object
+  if (validated) {
+    x
+  }
+}
