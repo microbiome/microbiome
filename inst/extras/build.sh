717533b25bef341668ed232de4c8e631404c7d30
antagomir <leo.lahti@iki.fi>
2016-10-04 23:10:49 +0200

    ok

--- inst/extras/build.sh
+++ inst/extras/build.sh
@@ -1,6 +1,6 @@
 # https://support.rstudio.com/hc/en-us/articles/200486518-Customizing-Package-Build-Options
-/home/lei/bin/R CMD BATCH document.R
-/home/lei/bin/R CMD build ../../ --no-build-vignettes --no-examples
-/home/lei/bin/R CMD check microbiome_0.99.87.tar.gz --no-build-vignettes --no-examples
-/home/lei/bin/R CMD INSTALL microbiome_0.99.87.tar.gz 
+/usr/local/bin/R CMD BATCH document.R
+/usr/local/bin/R CMD build ../../ --no-build-vignettes --no-examples
+/usr/local/bin/R CMD check microbiome_0.99.87.tar.gz --no-build-vignettes --no-examples
+/usr/local/bin/R CMD INSTALL microbiome_0.99.87.tar.gz 
 #/usr/local/bin/R CMD BiocCheck microbiome_0.99.32.tar.gz 
