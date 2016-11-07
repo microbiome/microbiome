bf8552772e8f3e3d836dc89cde89c91ef07b4981
antagomir <leo.lahti@iki.fi>
2016-11-08 00:42:10 +0100

    fixing ghpage pus

--- .push_gh_pages.sh
+++ .push_gh_pages.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Modified from:
+# https://rmflight.github.io/posts/2014/11/travis_ci_gh_pages.html
+
+# Completely overwrites gh-pages
+# would be also possible to pull and modify instead
 rm -rf out || exit 0;
 mkdir out;
 
@@ -14,8 +19,9 @@ done
 cd out
 git init
 git config user.name "microbiome-travis"
-git config user.email "leo.lahti@iki.fi"
-cp ../microbiome/inst/doc/vignette.html index.html
+git config user.email "travis"
+#cp ../microbiome/inst/doc/vignette.html index.html
+cp ../microbiome/vignettes/vignette.html index.html
 
 git add .
 git commit -m "deployed to github pages"
