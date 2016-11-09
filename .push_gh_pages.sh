74c8f20f94f57a5c63c86830afe16482a03c53c4
antagomir <leo.lahti@iki.fi>
2016-11-09 19:27:53 +0100

    ok

--- .push_gh_pages.sh
+++ .push_gh_pages.sh
@@ -19,13 +19,13 @@ done
 
 cd out
 git init
-#git config user.name "microbiome-travis"
-git config user.name "antagomir"
+git config user.name "microbiome-travis"
 git config user.email "travis"
 #cp ../microbiome/inst/doc/vignette.html index.html
-cp ../microbiome/vignettes/vignette.html index.html
+#cp ../microbiome/vignettes/vignette.html index.html
+touch index.html
 
 git add .
-git commit -m "deployed to github pages"
+git commit -m "Deployed to github pages"
 git push --force --quiet $FULL_REPO master:gh-pages
-echo "PUSH OK\n"
+
