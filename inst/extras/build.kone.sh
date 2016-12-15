b938d60a8f0bf46a97abda2c49d46c7d861df5d4
antagomir <leo.lahti@iki.fi>
2016-12-15 11:14:47 +0100

    ok

--- inst/extras/build.kone.sh
+++ inst/extras/build.kone.sh
@@ -1,6 +1,6 @@
 # https://support.rstudio.com/hc/en-us/articles/200486518-Customizing-Package-Build-Options
 /usr/bin/R CMD BATCH document.R
 /usr/bin/R CMD build ../../ --no-build-vignettes --no-examples
-/usr/bin/R CMD check microbiome_0.99.87.tar.gz --no-build-vignettes --no-examples
-/usr/bin/R CMD INSTALL microbiome_0.99.87.tar.gz 
+/usr/bin/R CMD check microbiome_0.99.88.tar.gz --no-build-vignettes --no-examples
+/usr/bin/R CMD INSTALL microbiome_0.99.88.tar.gz 
 #/usr//bin/R CMD BiocCheck microbiome_0.99.32.tar.gz 
