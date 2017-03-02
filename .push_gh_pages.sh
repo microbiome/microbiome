f3c3925594cf9f181f5a86673828b13938ec5cbb
antagomir <leo.lahti@iki.fi>
2017-03-02 11:43:07 +0100

    ok

--- .push_gh_pages.sh
+++ .push_gh_pages.sh
@@ -8,9 +8,29 @@
 rm -rf out; || exit 0;
 mkdir out;
 
+#SOURCE_BRANCH="master"
+#TARGET_BRANCH="gh-pages"
 
+# Can add separate vignette build here later
+#function doCompile {
+#  ./compile.sh
+#}
+# Run our compile script -> Add this to a suitable place
+# doCompile
+
+
+# Pull requests and commits to other branches shouldn't try to deploy, just build to verify
+#if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
+#    echo "Skipping deploy; just doing a build."
+#    doCompile
+#    exit 0
+#fi
+
+# Save some useful information
+#REPO=`git config remote.origin.url`
+#SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
+#SHA=`git rev-parse --verify HEAD`
 GH_REPO="@github.com/microbiome/microbiome.git"
-
 FULL_REPO="https://$GH_TOKEN$GH_REPO"
 
 for files in '*.tar.gz'; do
