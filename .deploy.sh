587b304d4ae33a122bcdd8cbf362e89d546bc39b
antagomir <leo.lahti@iki.fi>
2017-03-04 21:43:39 +0100

    ok

--- .deploy.sh
+++ .deploy.sh
@@ -17,7 +17,9 @@ git config --global user.name "Travis CI"
 cd public
 git init
 touch index.html
-git add .
+git add index.html
 git commit -a -m "Deploy to Github Pages"
-git push --force --quiet "https://${GITHUB_TOKEN}@$github.com/${GITHUB_REPO}.git" master:gh-pages > /dev/null 2>&1
+#git push --force --quiet "https://${GITHUB_TOKEN}@$github.com/${GITHUB_REPO}.git" master:gh-pages > /dev/null 2>&1
+# git push --force --quiet $FULL_REPO master:gh-pages
+git push --force "https://${GITHUB_TOKEN}@$github.com/${GITHUB_REPO}.git" master:gh-pages #> /dev/null 2>&1
 
