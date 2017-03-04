ba2ecfb7893d294a146bbbd2f5eefcfa6238fe64
antagomir <leo.lahti@iki.fi>
2017-03-04 21:19:18 +0100

    ok

--- .deploy.sh
+++ .deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # See https://medium.com/@nthgergo/publishing-gh-pages-with-travis-ci-53a8270e87db
 
-#set -o errexit
+#set -o errexit # put back
 
 rm -rf public
 mkdir public
@@ -16,7 +16,8 @@ git config --global user.name "Travis CI"
 # deploy
 cd public
 git init
+touch index.html
 git add .
-git commit -m "Deploy to Github Pages"
+git commit -a -m "Deploy to Github Pages"
 git push --force --quiet "https://${GITHUB_TOKEN}@$github.com/${GITHUB_REPO}.git" master:gh-pages > /dev/null 2>&1
 
