af3ce560088091320288403d5625c2ecb9ef17e5
antagomir <leo.lahti@iki.fi>
2017-03-04 13:11:50 +0100

    deploy test

--- .deploy.sh
+++ .deploy.sh
@@ -1,30 +1,21 @@
 #!/bin/bash
+# See https://medium.com/@nthgergo/publishing-gh-pages-with-travis-ci-53a8270e87db
+set -o errexit
 
-set -o errexit -o nounset
+rm -rf public
+mkdir public
 
-if [ "$TRAVIS_BRANCH" != "master" ]
-then
-  echo "This commit was made against the $TRAVIS_BRANCH and not the master! No deploy!"
-  exit 0
-fi
+# config
+git config --global user.email "nobody@nobody.org"
+git config --global user.name "Travis CI"
 
-rev=$(git rev-parse --short HEAD)
+# build (CHANGE THIS)
+# make
 
-cd stage/_book
-
+# deploy
+cd public
 git init
-git config user.name "antagomir"
-git config user.email "leo.lahti@iki.fi"
-
-git remote add upstream "https://$GH_TOKEN@github.com/microbiome/microbiome.git"
-git fetch upstream
-git reset upstream/gh-pages
-
-echo "microbiome.com" > index.html
-
-touch .
-
-git add -A .
-git commit -m "rebuild pages at ${rev}"
-git push -q upstream HEAD:gh-pages
+git add .
+git commit -m "Deploy to Github Pages"
+git push --force --quiet "https://${GITHUB_TOKEN}@$github.com/${GITHUB_REPO}.git" master:gh-pages > /dev/null 2>&1
 
