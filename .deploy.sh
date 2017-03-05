16135810136147006272f1b4b362af1958e5ed96
antagomir <leo.lahti@iki.fi>
2017-03-05 16:30:30 +0100

    ok

--- .deploy.sh
+++ .deploy.sh
@@ -30,7 +30,8 @@ git init
 
 #cp ../vignettes/vignette.html index.html
 #cp ../vignettes/*.html .
-cp ../inst/doc/*.html .
+#cp ../inst/doc/*.html .
+ls ../inst/doc > index.html
 git add *.html
 git commit -a -m "Deploy to Github Pages"
 git push --force --quiet $FULL_REPO master:gh-pages # > /dev/null 2>&1
