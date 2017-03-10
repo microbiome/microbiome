3a77ea25e464d4f1fbeec9a1a8d83cb46ff79083
Sudarshan <sudarshanshetty9@gmail.com>
2017-03-10 11:13:51 +0100

    save_tables.R
    Wrapper to save files with taxonomic, OTU or metadata information from Phyloseq object.

---
+++ R/save_tables.R
@@ -0,0 +1,28 @@
+#' @title  Saving Various Files as Comma Seperated Files
+#' @description This command will save the otu,taxonomy and meta data as *.csv file for manual inspection.
+#' @param x \code{\link{phyloseq-class}} object
+#' @param type "OTU" or "TAXA" or METADATA
+#' @return  \code{\link{excel}}
+#' @export
+#' @examples \dontrun{
+#'   data(DynamicsIBD)
+#'   p0 <- DynamicsIBD
+#'   p0.f <- format_phyloseq(p0)
+#'   save_tables(p0.f, "OTU")
+#'   save_tables(p0.f, "TAXA")
+#'   save_tables(p0.f, "METADATA")
+#'  }
+#' @keywords utilities
+save_tables <- function(x, type){
+  if (type == "OTU"){
+    y <- as.data.frame(x@otu_table);
+    write.csv(y, file = "otu_table.csv", fileEncoding = "UTF-16LE")
+  } else if (type == "TAXA"){
+    y <- as.data.frame(x@tax_table);
+    write.csv(y, file ="taxa_table.csv", fileEncoding = "UTF-16LE")
+  } else if (type == "METADATA"){
+    y <- as.data.frame(x@sam_data);
+    write.csv(y, file = "meta_table.csv", fileEncoding = "UTF-16LE")
+  }
+  print(getwd())
+}
