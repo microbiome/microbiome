e69c0720754e426f440f0b3cfd1ab17634bfcb73
antagomir <leo.lahti@iki.fi>
2017-04-09 20:31:37 +0200

    speed up vignettes

---
+++ R/coverage.R
@@ -0,0 +1,31 @@
+#' @title Coverage Index
+#' @description Community coverage index.
+#' @details The coverage index gives the number of groups needed to
+#'   have a given proportion of the ecosystem occupied (by default 0.5 ie 50%). 
+#' @param threshold Indicates the fraction  of the ecosystem to be occupied by the N most abundant species (N is returned by this function).
+#' @inheritParams global
+#' @return A vector of coverage indices
+#' @export
+#' @examples
+#'   data(dietswap)
+#'   d <- coverage(dietswap, threshold = 0.5)
+#' @author Contact: Leo Lahti \email{microbiome-admin@@googlegroups.com}
+#' @seealso dominance, global
+#' @keywords utilities
+coverage <- function(x, threshold = 0.5, split = TRUE) {
+
+  # Pick the OTU data
+  otu <- abundances(x)
+
+  if (!split) {
+    otu <- as.matrix(rowSums(otu)/sum(otu), nrow = nrow(otu))
+  }
+
+  # Number of groups needed to have 50% of the ecosystem occupied
+  do <- apply(otu, 2, function (x) {min(which(cumsum(rev(sort(x/sum(x)))) >= threshold))})
+
+  names(do) <- sample_names(x)
+  
+  do
+
+}
---
+++ R/rare_abundance.R
@@ -0,0 +1,20 @@
+#' @title Rare Abundance Index
+#' @description Calculates the community rare_abundance index.
+#' @inheritParams core_abundance
+#' @return A vector of rare_abundance indices
+#' @export
+#' @examples
+#'   data(dietswap)
+#'   d <- rare_abundance(dietswap, detection = 0.1/100, prevalence = 50/100)
+#' @details The rare_abundance index gives the relative proportion of rare species (ie. those that are not part of the core microbiota) in the interval [0,1]. This is the complement (1-x) of the core abundance.
+#' The rarity function provides the abundance of the least abundant taxa
+#' within each sample, regardless of the population prevalence.
+#' @author Contact: Leo Lahti \email{microbiome-admin@@googlegroups.com}
+#' @keywords utilities
+#' @seealso core_abundance, rarity, diversity
+rare_abundance <- function(x, detection = .1/100, prevalence = 50/100, split = TRUE) {
+
+  1 - core_abundance(x, detection, prevalence, split)
+
+}
+
