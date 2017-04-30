9db8c7ac1d603d2e48acb5a4bbc66aac41a6b610
Sudarshan <sudarshanshetty9@gmail.com>
2017-04-30 22:46:05 +0200

    Update read_phyloseq
    Added support for converting different formats of OTU tables obtained from Mothur (shared and consensus taxonomy files), QIIME (biom and mapping files), and simple ".csv" format (otu_table, mapping and taxonomy files) into a phyloseq object. This is the simplified way to convert different data sets into a phyloseq object. Later we can also include ability to merge phylogenetic tree. However, at the moment this is easily done in phyloseq.

--- R/read_phyloseq.R
+++ R/read_phyloseq.R
@@ -3,24 +3,28 @@
 #' @param otu.file File containing the OTU table (for mothur this is the file with the .shared extension)
 #' @param taxonomy.file (for mothur this is typically the consensus taxonomy file with the .taxonomy extension)
 #' @param metadata.file File containing samples x variables metadata.
-#' @param type Input data type: "mothur" implemented now.
+#' @param type Input data type: "mothur" or "simple" or "biom" type.
 #' @return \code{\link{phyloseq-class}} object
 #' @export
 #' @details See help(read_mothur2phyloseq) for details on the Mothur input format.
 #' @examples \dontrun{
-#'   pseq <- read_phyloseq(...)
+#'   pseq <- read_phyloseq(otu.file = NULL, taxonomy.file = NULL, metadata.file = NULL, type = c("mothur", "simple", "biom"))
 #'  }
 #' @author Sudarshan A. Shetty \email{sudarshanshetty9@@gmail.com}
 #' @keywords utilities
-read_phyloseq <- function(otu.file = NULL, taxonomy.file = NULL, metadata.file = NULL, type = "mothur"){
-
-  # TODO make read_phyloseq for CSV files outputted by write_phyloseq function
-  # TODO add automated recognition of the type?
-
-  if (type == "mothur") {
-    pseq <- read_mothur2phyloseq(otu.file, taxonomy.file, metadata.file)
-  }
-
-  pseq
+read_phyloseq <- function(otu.file = NULL, taxonomy.file = NULL, metadata.file = NULL, type = c("mothur", "simple", "biom")){
   
-}
+  # TODO add automated recognition of the type?
+  
+  if (type == "mothur") {
+    pseq.mothur <- read_mothur2phyloseq(otu.file, taxonomy.file, metadata.file)
+    return(pseq.mothur)
+  } else if (type == "simple"){
+    pseq.sim <- read_csv2phyloseq(otu.file, taxonomy.file, metadata.file)
+    return(pseq.sim)
+  } else if (type == "biom"){
+    pseq.biom <- read_biom2phyloseq(otu.file, taxonomy.file, metadata.file)
+  }
+  return(pseq.biom)
+} 
+
