ebf419d5a4a6e6de115c8dd73435679fe1a73afc
antagomir <leo.lahti@iki.fi>
2017-05-17 02:04:51 +0200

    sync

--- .deploy.sh
+++ .deploy.sh
@@ -21,8 +21,6 @@ FULL_REPO="https://$GH_TOKEN$GH_REPO"
   #  ./compile.sh
 #}
 
-echo "test test"
-
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
 if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
     echo "Skipping deploy; just doing a build."
