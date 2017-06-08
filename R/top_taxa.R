806b8cc9302fa1943da84a38b23cd422030586ca
Leo Lahti <leo.lahti@utu.fi>
2017-06-08 15:30:06 +0200

    top_taxa fix

--- R/top_taxa.R
+++ R/top_taxa.R
@@ -10,6 +10,6 @@
 #' topx <- top_taxa(dietswap, n=10)
 top_taxa <- function(x, n=ntaxa(x)) {
     
-    names(sort(colSums(abundances(x)), decreasing=TRUE)[1:n])
+    names(sort(rowSums(abundances(x)), decreasing=TRUE)[1:n])
     
 }
