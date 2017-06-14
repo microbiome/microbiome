5381a2b01471b411a1344d8cd021e4e0ee0fda20
Leo Lahti <leo.lahti@utu.fi>
2017-06-14 18:48:06 +0200

    divergence checks

--- R/divergence.R
+++ R/divergence.R
@@ -17,14 +17,14 @@
 #' Subsampling or bootstrapping can be applied to equalize sample sizes
 #' between comparisons.
 #' 
-#' The anticorrelation mode is a simple indicator that returns
+#' The spearman mode is a simple indicator that returns
 #' average spearman correlation between samples of the input data and
 #' the overall group-wise average. The inverse of this measure
-#' (ie cor instead of 1-cor as in here) was used in Salonen et al. (2014)
+#' (ie rho instead of 1-rho as in here) was used in Salonen et al. (2014)
 #' to quantify group homogeneity.
 #' 
 #' @param x phyloseq object 
-#' @param method dissimilarity method ('anticorrelation' or any method
+#' @param method dissimilarity method ('spearman' or any method
 #' available via the vetan::vegdist function)
 #' @return Vector with dissimilarities; one for each sample, quantifying the
 #' dissimilarity of the sample from the group-level mean.
@@ -39,7 +39,7 @@
 #' The inter- and intra-individual homogeneity measures used in
 #' Salonen et al. ISME J. 8:2218-30, 2014 were obtained as
 #' 1 - beta where beta is the group diversity as quantified by the
-#' anticorrelation method.
+#' spearman method.
 #' 
 #' To cite this R package, see citation('microbiome')
 #' 
@@ -47,15 +47,15 @@
 #' standard beta diversity measures
 #' @author Contact: Leo Lahti \email{microbiome-admin@@googlegroups.com}
 #' @keywords utilities
-divergence <- function(x, method="anticorrelation") {
+divergence <- function(x, method="spearman") {
     
     # Abundance matrix (taxa x samples)
     x <- abundances(x)
     
-    if (method == "anticorrelation") {
-        b <- anticorrelation(x, "spearman")
-    } else if (method == "meanbeta") {
-        b <- beta(x, method="bray")
+    if (method == "spearman") {
+        b <- spearman(x, "spearman")
+    } else if (method == "bray") {
+        b <- beta.mean(x, method="bray")
     }
     
     b
@@ -63,7 +63,7 @@ divergence <- function(x, method="anticorrelation") {
 }
 
 
-anticorrelation <- function(x, method="spearman") {
+spearman <- function(x, method="spearman") {
     
     # Correlations calculated against the mean of the sample set
     cors <- as.vector(cor(x, matrix(rowMeans(x)),
@@ -76,15 +76,43 @@ anticorrelation <- function(x, method="spearman") {
 
 
 
-beta <- function(x, method="bray") {
+beta.mean <- function(x, method="bray") {
     
-    # Correlations calculated against the mean of the sample set
+    # Divergence calculated against the mean of the sample set
     b <- c()
     m <- rowMeans(x)
     for (i in 1:ncol(x)) {
         xx <- rbind(x[, i], m)
         xxx <- vegdist(xx, method=method)
         b[[i]] <- as.matrix(xxx)[1, 2]
+    }
+    
+    b
+    
+}
+
+
+
+
+
+beta.pairs <- function(x, method="bray", n = ncol(x)) {
+    
+    # Divergence calculated against the mean of the sample set
+    b <- c()
+
+    # All index pairs
+    # pairs <- combn(1:ncol(x), 2)
+
+    # Pick n pairs without replacement
+    # inds <- sample(ncol(pairs), n)
+
+    # Distance between each sample and a random pair
+    for (i in 1:ncol(x)) {
+        i2 <- sample(setdiff(1:ncol(x), i), 1)
+        #i1 <- pairs[1, i]
+        #i2 <- pairs[2, i]	
+        xx <- t(x[, c(i, i2)])
+        b[[i]] <- as.vector(vegdist(xx, method=method)) 
     }
     
     b
