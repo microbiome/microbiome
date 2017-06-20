cbba76356fe2a8dbca8d7012cd2249f81faa24a7
Leo Lahti <leo.lahti@utu.fi>
2017-06-20 14:55:41 +0200

    closes #42

---
+++ R/remove_samples.R
@@ -0,0 +1,28 @@
+#' @title Exclude Samples
+#' @description Filter out selected samples from a phyloseq object.
+#' @param samples Names of samples to be removed.
+#' @param x \code{\link{phyloseq-class}} object
+#' @return Filtered phyloseq object.
+#' @references
+#' To cite the microbiome R package, see citation('microbiome') 
+#' @author Contact: Leo Lahti \email{microbiome-admin@@googlegroups.com}
+#' @keywords utilities
+#' @export
+#' @seealso phyloseq::prune_samples, phyloseq::subset_samples
+#' @details This complements the phyloseq function prune_samples by providing a way to exclude given groups from a phyloseq object.
+#' @examples
+#' data(dietswap)
+#' pseq <- remove_samples(c("Akkermansia", "Dialister"), dietswap)
+remove_samples <- function(samples = NULL, x) {
+
+    if (is.null(samples)) {return(x)}	     
+    keep <- setdiff(sample_names(x), samples)
+    prune_samples(keep, x)
+    
+}
+
+
+
+
+
+
