33d1dee7b8998b579e79d48020590df4f18d6634
antagomir <leo.lahti@iki.fi>
2017-07-14 20:51:37 +0200

    made the read functions executable now - closing #50

--- R/read_csv2phyloseq.R
+++ R/read_csv2phyloseq.R
@@ -9,26 +9,21 @@
 #' @return  \code{\link{phyloseq-class}} object.
 #' @export
 #' @examples
-#' \dontrun{
-#' # TODO: add example files in inst/extdata/
-#' # and read here with
-#' # system.file('inst/extdata/..', package='microbiome') command
-#' # To make this example executable
 #'
-#' otu.file <- "inst/extdata/qiita1629_otu_table.csv"
-#' qiita1629_otu_table <- system.file(otu.file, package='microbiome')
+#' otu.file <-
+#'    system.file("inst/extdata/qiita1629_otu_table.csv",
+#'        package='microbiome')
 #'
-#' tax.file <- "inst/extdata/qiita1629_taxonomy_table.csv"
-#' qiita1629_taxonomy_table <- system.file(tax.file, package='microbiome')
+#' tax.file <- system.file("inst/extdata/qiita1629_taxonomy_table.csv",
+#'        package='microbiome')
 #'
-#' meta.file <- "inst/extdata/qiita1629_mapping_subset.csv"
-#' qiita1629_mapping_subset <- system.file(meta.file, package='microbiome')
+#' meta.file <- system.file("inst/extdata/qiita1629_mapping_subset.csv",
+#'        package='microbiome')
 #'
 #' p0 <- read_csv2phyloseq(
-#'      otu.file=otu.file, 
-#'      taxonomy.file=tax.file, 
-#'      metadata.file=meta.file)
-#' }
+#'          otu.file=otu.file, 
+#'          taxonomy.file=tax.file, 
+#'          metadata.file=meta.file)
 #'
 #' @author Sudarshan A. Shetty \email{sudarshanshetty9@@gmail.com}
 #' @keywords utilities
