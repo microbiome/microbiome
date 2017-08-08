eff9481f4609618fe3d1dadb98f70f5ab8555e52
antagomir <leo.lahti@iki.fi>
2017-08-09 00:04:43 +0300

    polishing tabs and spaces

--- R/read_biom2phyloseq.R
+++ R/read_biom2phyloseq.R
@@ -12,25 +12,25 @@
 #' @examples
 #'
 #' biom.file <- 
-#'  system.file("extdata/qiita1629.biom", 
-#'              package = "microbiome")
+#'    system.file("extdata/qiita1629.biom", 
+#'            package = "microbiome")
 #'
 #' meta.file <- 
-#'  system.file("extdata/qiita1629_mapping.csv", 
-#'              package = "microbiome")
+#' system.file("extdata/qiita1629_mapping.csv", 
+#'            package = "microbiome")
 #'
 #' p0 <- read_biom2phyloseq(otu.file = biom.file, 
-#'                         metadata.file = meta.file, 
-#'                         taxonomy.file = NULL)
+#'                        metadata.file = meta.file, 
+#'                        taxonomy.file = NULL)
 #'
 #' @author Sudarshan A. Shetty \email{sudarshanshetty9@@gmail.com}
 #' @keywords utilities
 read_biom2phyloseq <- function(otu.file = NULL, 
-                               taxonomy.file = NULL, metadata.file = NULL)
+                            taxonomy.file = NULL, metadata.file = NULL)
 {
   levels <- c("Domain", "Phylum", 
-              "Class", "Order", "Family", 
-              "Genus")
+            "Class", "Order", "Family", 
+            "Genus")
   
   otu_biom <- import_biom(otu.file, 
                           parseFunction = parse_taxonomy_default)
@@ -38,16 +38,12 @@ read_biom2phyloseq <- function(otu.file = NULL,
                   check.names = FALSE, row.names = 1)
   s.map <- sample_data(map)
   phyobj <- merge_phyloseq(otu_biom, 
-                           s.map)
-  if (ncol(tax_table(phyobj)) == 
-      6)
-  {
+                        s.map)
+  if (ncol(tax_table(phyobj)) == 6) {
     colnames(tax_table(phyobj)) <- levels
     
     return(phyobj)
-  } else if (ncol(tax_table(phyobj)) == 
-             7)
-  {
+  } else if (ncol(tax_table(phyobj)) == 7) {
     colnames(tax_table(phyobj)) <- c(levels, 
                                      "Species")
   }
