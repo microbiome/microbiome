451a1256286110121a8549b188a467e32505b585
antagomir <leo.lahti@iki.fi>
2017-09-06 09:08:23 +0300

    Package size OK for Bioc

--- inst/extras/build.sh
+++ inst/extras/build.sh
@@ -1,7 +1,7 @@
 # https://support.rstudio.com/hc/en-us/articles/200526518-Customizing-Package-Build-Options
 #/usr/bin/R CMD BATCH document.R
 /usr/bin/R CMD build ../../ #--no-examples  --no-build-vignettes 
-/usr/bin/R CMD check microbiome_0.99.71.tar.gz #--no-build-vignettes --no-examples
-/usr/bin/R CMD BiocCheck microbiome_0.99.71.tar.gz
-/usr/bin/R CMD INSTALL microbiome_0.99.71.tar.gz 
+/usr/bin/R CMD check microbiome_0.99.72.tar.gz #--no-build-vignettes --no-examples
+/usr/bin/R CMD BiocCheck microbiome_0.99.72.tar.gz
+/usr/bin/R CMD INSTALL microbiome_0.99.72.tar.gz 
 
