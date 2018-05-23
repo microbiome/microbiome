1b60a55fbf0f5b766c83ab47bfdb134ff344b59b
antagomir <leo.lahti@iki.fi>
2018-05-23 17:27:37 +0200

    ok

--- Edu/main.R
+++ Edu/main.R
@@ -1,9 +1,10 @@
 library(rmarkdown)
-render("univariate.Rmd")
-render("lm.Rmd")
-render("pooled.Rmd")
 render("all.Rmd")
-render("extra.Rmd")
+
+#render("univariate.Rmd")
+#render("lm.Rmd")
+#render("pooled.Rmd")
+#render("extra.Rmd")
 
 
 
