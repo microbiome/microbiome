bdf8b8917b81c3314aae120d2de00067da7ff699
Leo <leo.lahti@iki.fi>
2019-05-27 10:15:23 +0300

    Clarified doc and examples - fixes #116

--- R/core.R
+++ R/core.R
@@ -3,8 +3,9 @@
 #' @param x \code{\link{phyloseq-class}} object
 #' @param detection Detection threshold for absence/presence
 #' (strictly greater by default).
-#' @param prevalence Prevalence threshold (in [0, 1];
-#' strictly greater by default)
+#' @param prevalence Prevalence threshold (in [0, 1]). The
+#' required prevalence is strictly greater by default. To include the
+#' limit, set include.lowest to TRUE. 
 #' @param include.lowest Include the lower boundary of the detection and
 #' prevalence cutoffs. FALSE by default.
 #' @param ... Arguments to pass.
@@ -24,6 +25,11 @@
 #' # Detection threshold 0 (strictly greater by default);
 #' # Prevalence threshold 50 percent (strictly greater by default)
 #' pseq <- core(dietswap, 0, 50/100)
+#' # Detection threshold 0 (strictly greater by default);
+#' # Prevalence threshold exactly 100 percent; for this set
+#' # include.lowest=TRUE, otherwise the required prevalence is 
+#' # strictly greater than 100
+#' pseq <- core(dietswap, 0, 100, include.lowest = TRUE)
 core <- function(x, detection, prevalence, include.lowest=FALSE) {
     
     xorig <- x
