06f39f9ef7a7289740c21f59620bd2b4c3a2925b
Sudarshan <sudarshanshetty9@gmail.com>
2020-10-27 19:06:56 +0100

    landscape plot improvements
    access to color and size options for user to modify default colors

--- R/plot_landscape.R
+++ R/plot_landscape.R
@@ -1,7 +1,7 @@
 #' @title Landscape Plot
 #' @description Wrapper for visualizing sample similarity landscape
 #' ie. sample density in various 2D projections.
-#' @param x \code{\link{phyloseq-class}} object or a data matrix 
+#' @param x \code{\link{phyloseq-class}} object or a data matrix
 #' (samples x features; eg. samples vs. OTUs). If the input x is a 2D matrix
 #' then it is plotted as is.
 #' @param method Ordination method, see phyloseq::plot_ordination; or "PCA",
@@ -14,10 +14,13 @@
 #' @param x.ticks Number of ticks on the X axis
 #' @param rounding Rounding for X axis tick values
 #' @param add.points Plot the data points as well
+#' @param point.opacity Transparency-level for points
 #' @param adjust Kernel width adjustment
 #' @param size point size
 #' @param legend plot legend TRUE/FALSE
 #' @param shading Add shading in the background.
+#' @param shading.low Color for shading low density regions
+#' @param shading.high Color for shading high density regions
 #' @return A \code{\link{ggplot}} plot object.
 #' @export
 #' @details For consistent results, set random seet (set.seed) before
@@ -39,10 +42,20 @@
 #' p <- plot_landscape(dietswap, method = "PCA", transformation = "clr")
 #'
 #' @keywords utilities
-plot_landscape <- function(x, method="PCoA", distance="bray",
-    transformation = "identity", 
-    col=NULL, main=NULL, x.ticks=10, rounding=0, add.points=TRUE,
-    adjust=1, size=1, legend=FALSE, shading=TRUE) {
+plot_landscape <- function(x, method="PCoA",
+                           distance="bray",
+                           transformation = "identity",
+                           col=NULL,
+                           main=NULL,
+                           x.ticks=10,
+                           rounding=0,
+                           add.points=TRUE,
+                           adjust=1, size=1,
+                           legend=FALSE,
+                           shading=TRUE,
+                           shading.low="#ebf4f5",
+                           shading.high="#e9b7ce",
+                           point.opacity=0.75) {
 
     if (is.matrix(x) || is.data.frame(x)) {
 
@@ -70,7 +83,7 @@ plot_landscape <- function(x, method="PCoA", distance="bray",
 
             # TODO add robust PCA
             #(pc.rob <- princomp(stackloss, covmat = MASS::cov.rob(stackloss)))
-        
+
         } else if (method == "t-SNE") {
 
             dm <- vegdist(t(otu_table(x)), distance)
@@ -81,14 +94,14 @@ plot_landscape <- function(x, method="PCoA", distance="bray",
             rownames(proj) <- sample_names(x)
 
         } else {
-    
+
             #quiet(proj <- get_ordination(x, method, distance))
             quiet(x.ord <- ordinate(x, method, distance))
             # Pick the projected data (first two columns + metadata)
             quiet(proj <- phyloseq::plot_ordination(x, x.ord, justDF=TRUE))
             # Rename the projection axes
-        } 
-        
+        }
+
     }
 
     proj <- as.data.frame(proj)
@@ -105,14 +118,17 @@ plot_landscape <- function(x, method="PCoA", distance="bray",
     }
 
     p <- densityplot(proj[, c(1,2)], main=NULL, x.ticks=10,
-            rounding=0, add.points=add.points, 
+            rounding=0, add.points=add.points,
             adjust=1, size=size, col=proj$col,
             legend = TRUE,
-            shading=shading) +
+            shading=shading,
+            shading.low=shading.low,
+            shading.high=shading.high,
+            point.opacity=point.opacity) +
             guides(color = guide_legend(title = guide.title))
 
     p
-    
+
 }
 
 
@@ -127,38 +143,44 @@ plot_landscape <- function(x, method="PCoA", distance="bray",
 #' @param x.ticks Number of ticks on the X axis
 #' @param rounding Rounding for X axis tick values
 #' @param add.points Plot the data points as well
+#' @param point.opacity Transparency-level for points
 #' @param col Color of the data points. NAs are marked with darkgray.
 #' @param adjust Kernel width adjustment
 #' @param size point size
 #' @param legend plot legend TRUE/FALSE
 #' @param shading Shading
+#' @param shading.low Color for shading low density regions
+#' @param shading.high Color for shading high density regions
 #' @return ggplot2 object
 #' @examples
 #' # p <- densityplot(cbind(rnorm(100), rnorm(100)))
-#' @references See citation('microbiome') 
+#' @references See citation('microbiome')
 #' @author Contact: Leo Lahti \email{microbiome-admin@@googlegroups.com}
 #' @keywords utilities
 densityplot <- function(x, main=NULL, x.ticks=10, rounding=0,
     add.points=TRUE, col="black",
     adjust=1, size=1,
-    legend=FALSE, shading=TRUE) {
+    legend=FALSE, shading=TRUE,
+    shading.low="white",
+    shading.high="black",
+    point.opacity=0.75) {
 
     df <- x
     if (!is.data.frame(df)) {
         df <- as.data.frame(as.matrix(df))
     }
-    
+
     # Avoid warnings
     x <- y <- ..density.. <- color <- NULL
-    
+
     # If colors are NA then mark them dark gray
     lev <- levels(col)
     if (!is.numeric(col) & any(is.na(col))) {
         col <- as.character(col)
         col[unname(which(is.na(col)))] <- "darkgray"
-        col <- factor(col, levels = unique(c(lev, "darkgray")))    
+        col <- factor(col, levels = unique(c(lev, "darkgray")))
     }
-    
+
     xvar <- colnames(df)[[1]]
     yvar <- colnames(df)[[2]]
     df[["x"]] <- df[, 1]
@@ -166,62 +188,68 @@ densityplot <- function(x, main=NULL, x.ticks=10, rounding=0,
 
     df[["color"]] <- col
     df[["size"]] <- size
-    
+
     # Remove NAs
     df <- df[!(is.na(df[["x"]]) | is.na(df[["y"]])), ]
-    
+
     # Determine bandwidth for density estimation
     bw <- adjust * c(bwi(df[["x"]]), bwi(df[["y"]]))
     if (any(bw == 0)) {
-        warning("Zero bandwidths 
-                (possibly due to small number of observations). 
+        warning("Zero bandwidths
+                (possibly due to small number of observations).
                 Using minimal bandwidth.")
         bw[bw == 0]=bw[bw == 0] + min(bw[!bw == 0])
     }
-    
+
     # Construct the figure
     p <- ggplot(df)
 
     if (shading) {
         p <- p + stat_density2d(aes(x, y, fill=..density..),
-                geom="raster", h=bw, 
+                geom="raster", h=bw,
                 contour=FALSE)
-        p <- p + scale_fill_gradient(low="white", high="black")             
+        p <- p + scale_fill_gradient(low=shading.low, high=shading.high)
     }
-    
+
     if (add.points) {
 
-        if (length(unique(df$color)) == 1 && length(unique(df$size)) == 1) {    
+        if (length(unique(df$color)) == 1 && length(unique(df$size)) == 1) {
             p <- p + geom_point(aes(x=x, y=y),
-            col=unique(df$color), size=unique(df$size))
+            col=unique(df$color), size=unique(df$size),
+            alpha=point.opacity)
         } else if (length(unique(df$color)) == 1 &&
             length(unique(df$size)) > 1) {
             p <- p + geom_point(aes(x=x, y=y, size=size),
-            col=unique(df$color))
+            col=unique(df$color),
+            alpha=point.opacity)
         } else if (length(unique(df$color)) > 1 &&
             length(unique(df$size)) == 1) {
             p <- p + geom_point(aes(x=x, y=y, col=color),
-            size=unique(df$size))
+            size=unique(df$size),
+            alpha=point.opacity)
         } else {
-            p <- p + geom_point(aes(x=x, y=y, col=color, size=size))
+            p <- p + geom_point(aes(x=x, y=y, col=color, size=size),
+                                alpha=point.opacity)
         }
     }
-    
+
     p <- p + xlab(xvar) + ylab(yvar)
-    
+
     if (!legend) {
         p <- p + theme(legend.position="none")
     }
-    
+
     p <- p + scale_x_continuous(breaks=round(seq(floor(min(df[["x"]])),
         ceiling(max(df[["x"]])), length=x.ticks), rounding))
-    
+
     if (!is.null(main)) {
         p <- p + ggtitle(main)
     }
-    
-    p
-    
+
+    p + theme(panel.grid = element_blank(),
+              panel.background = element_rect(fill = "white", color="grey70"),
+              legend.key=element_blank())
+
 }
 
 
