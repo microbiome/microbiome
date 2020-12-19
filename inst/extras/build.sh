d4d3dae0bf709bd57bd11d39ac14e97e4b32ff91
Leo <leo.lahti@iki.fi>
2020-12-20 01:40:39 +0200

    Chao1 now always bias corrected version - fixes #150

--- inst/extras/build.sh
+++ inst/extras/build.sh
@@ -2,6 +2,6 @@
 #~/bin/R-3.5.0/bin/R CMD BATCH document.R
 
 ~/bin/R-4.0.0/bin/R CMD build ../../ --resave-data #--no-examples  --no-build-vignettes 
-~/bin/R-4.0.0/bin/R CMD check microbiome_1.13.3.tar.gz #--no-build-vignettes --no-examples
-~/bin/R-4.0.0/bin/R CMD BiocCheck microbiome_1.13.3.tar.gz
-~/bin/R-4.0.0/bin/R CMD INSTALL microbiome_1.13.3.tar.gz 
+~/bin/R-4.0.0/bin/R CMD check microbiome_1.13.4.tar.gz #--no-build-vignettes --no-examples
+~/bin/R-4.0.0/bin/R CMD BiocCheck microbiome_1.13.4.tar.gz
+~/bin/R-4.0.0/bin/R CMD INSTALL microbiome_1.13.4.tar.gz 
