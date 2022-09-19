13d6317268ddb95cd0ba7622d5600a5bfc112d8f
removedporn <86824510+removedporn@users.noreply.github.com>
2022-09-19 19:41:31 +0800

    Delete R directory

--- R/is.phyloseq.R
+++
@@ -1,3 +0,0 @@
-is.phyloseq <- function (x) {
-    length(x) == 1 && is(x) == "phyloseq"
-}
--- R/read_taxtable.R
+++
@@ -1,9 +0,0 @@
-read_taxtable <- function (taxonomy.file, sep = ",") {
-
-    s.tax <- read.csv(taxonomy.file, row.names=1, check.names=FALSE, sep = sep)
-    s.taxmat <- as.matrix(s.tax)
-
-    tax_table(s.taxmat)
-
-}
-
